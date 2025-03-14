0c10313583588e34c0f2796a8367464ee72e151c
이준희 <0210leejun@gmail.com>
2025-03-14 19:16:06 +0900

    🔧 tailwindcss 및 svgr 관련 업데이트트

---
+++ tailwind.config.ts
@@ -0,0 +1,57 @@
+// import type { Config } from "tailwindcss";
+
+// export default {
+//   content: [
+//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         gray: {
+//           50: "#F7F7F7",
+//           100: "#EAEAEA",
+//           200: "#CCCCCC",
+//           300: "#999999",
+//           400: "#707070",
+//           500: "#474747",
+//           600: "#252525",
+//           700: "#1C1C1C",
+//           800: "#1A1A1A",
+//           900: "#161616",
+//         },
+//         point: "#FF7700",
+//       },
+//       fontFamily: {
+//         sans: ["Pretendard Variable", "Arial", "sans-serif"],
+//       },
+//       fontSize: {
+//         "display-28-b": ["28px", { lineHeight: "35px", fontWeight: "700" }],
+//         "display-24-b": ["24px", { lineHeight: "30px", fontWeight: "700" }],
+//         "display-24-m": ["24px", { lineHeight: "30px", fontWeight: "500" }],
+//         "headline-20-b": ["20px", { lineHeight: "25px", fontWeight: "700" }],
+//         "headline-20-m": ["20px", { lineHeight: "25px", fontWeight: "500" }],
+//         "subhead-16-sb": ["16px", { lineHeight: "20px", fontWeight: "600" }],
+//         "subhead-14-sb": ["14px", { lineHeight: "17.5px", fontWeight: "600" }],
+//         "subhead-12-sb": ["12px", { lineHeight: "15px", fontWeight: "600" }],
+//         "body-16-r": ["16px", { lineHeight: "20px", fontWeight: "400" }],
+//         "body-16-sb": ["16px", { lineHeight: "20px", fontWeight: "600" }],
+//         "body-14-r": ["14px", { lineHeight: "17.5px", fontWeight: "400" }],
+//         "caption-12-r": ["12px", { lineHeight: "15px", fontWeight: "400" }],
+//         "caption-10-l": ["10px", { lineHeight: "12.5px", fontWeight: "300" }],
+//       },
+//       screens: {
+//         xl: "1001px",
+//         lg: "1000px",
+//         md: "641px",
+//         sm: "393px",
+//         map: "784px",
+//       },
+//       boxShadow: {
+//         "custom-light": "0px 0px 10px 0px rgba(0, 0, 0, 0.1)",
+//       },
+//     },
+//   },
+//   plugins: [],
+// } satisfies Config;
